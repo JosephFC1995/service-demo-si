7ac97e744eb38f8c1a3be54539c9ac4622f61929
Joseph <josefc9512@gmail.com>
2020-08-28 20:35:53 -0500

    add updates

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
 	bodyParser.urlencoded({
 		limit: "10mb",
 		extended: true,
-		parameterLimit: 50000,
+		parameterLimit: 1000000000,
 	})
 );
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
